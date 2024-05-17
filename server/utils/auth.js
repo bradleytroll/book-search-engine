d49b37bebc78f300c71d283a05fd892d0b8f0013
Bradley Troll <bradleytroll@gmail.com>
2024-05-17 09:41:54 -0400

    Updates server-side auth.js file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,13 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+// Set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
+  // Function to sign a token
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
+  // Apollo Server context function
+  context: ({ req }) => {
+    // Allows token to be sent via req.query or headers
     let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
@@ -16,24 +22,96 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return { user: null };
     }
 
-    // verify token and get user data out of it
+    // Verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+      return { user: data };
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      return { user: null };
     }
-
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
+
+// const jwt = require('jsonwebtoken');
+
+// // Set token secret and expiration date
+// const secret = 'mysecretsshhhhh';
+// const expiration = '2h';
+
+// module.exports = {
+//   // Function to sign a token
+//   signToken: function ({ username, email, _id }) {
+//     const payload = { username, email, _id };
+//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+//   },
+
+//   // Apollo Server context function
+//   context: ({ req }) => {
+//     // Allows token to be sent via req.query or headers
+//     let token = req.query.token || req.headers.authorization;
+
+//     // ["Bearer", "<tokenvalue>"]
+//     if (req.headers.authorization) {
+//       token = token.split(' ').pop().trim();
+//     }
+
+//     if (!token) {
+//       return { user: null };
+//     }
+
+//     // Verify token and get user data out of it
+//     try {
+//       const { data } = jwt.verify(token, secret, { maxAge: expiration });
+//       return { user: data };
+//     } catch {
+//       console.log('Invalid token');
+//       return { user: null };
+//     }
+//   },
+// };
+
+
+// // const jwt = require('jsonwebtoken');
+
+// // // set token secret and expiration date
+// // const secret = 'mysecretsshhhhh';
+// // const expiration = '2h';
+
+// // module.exports = {
+// //   // function for our authenticated routes
+// //   authMiddleware: function (req, res, next) {
+// //     // allows token to be sent via  req.query or headers
+// //     let token = req.query.token || req.headers.authorization;
+
+// //     // ["Bearer", "<tokenvalue>"]
+// //     if (req.headers.authorization) {
+// //       token = token.split(' ').pop().trim();
+// //     }
+
+// //     if (!token) {
+// //       return res.status(400).json({ message: 'You have no token!' });
+// //     }
+
+// //     // verify token and get user data out of it
+// //     try {
+// //       const { data } = jwt.verify(token, secret, { maxAge: expiration });
+// //       req.user = data;
+// //     } catch {
+// //       console.log('Invalid token');
+// //       return res.status(400).json({ message: 'invalid token!' });
+// //     }
+
+// //     // send to next endpoint
+// //     next();
+// //   },
+// //   signToken: function ({ username, email, _id }) {
+// //     const payload = { username, email, _id };
+
+// //     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+// //   },
+// // };
